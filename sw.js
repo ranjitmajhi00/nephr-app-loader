36e041898907b7a499f1bf052fbbb0a811d8b21c
ranjitmajhi00 <ranjeetmajhi99@gmail.com>
2025-11-04 00:33:11 +0545

    Remove comments from service worker fetch event

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-// A very basic service worker for installability
-self.addEventListener('fetch', (event) => {
-  // We are not doing any caching for now, just making it installable.
-});
+self.addEventListener('fetch', (event) => {});
